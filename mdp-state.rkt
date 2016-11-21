2940afce3c2d7df98fa2c743bb65995283aa9c11
Guest <guest-ygxcvs@OS308-09.fs.uml.edu>
2016-11-20 21:06:29 -0500

    Updated mdp-state

--- mdp-state.rkt
+++ mdp-state.rkt
@@ -1,24 +1,27 @@
 #lang racket
 
+;; needed libraries
 (require "define-argcheck.rkt"
          "composer/composer.rkt")
 
 (provide (all-defined-out))
 
-(struct state [list-of-harmonies harmonic-progression state-value]
+;; state representation that contains a chord voicing, harmonic progression
+(struct state [chord-voicing harmonic-progression state-value]
     #:guard
-      (lambda (loh hp sv name)
+      (lambda (cv hp sv name)
         (if (and
-             (list? loh) ; list of harmonies
+             (list? cv) ; list of harmonies
              (list? hp) ; list of symbols
              (number? sv)) ; numeric state value
-             (values loh hp sv)
+             (values cv hp sv)
             (error "Invalid state parameters"))))
 
+;; create a note without duration (pitch)
 (define (pitch letter octave)
   (note letter octave null-beat))
 
-
+;; upper and lower bounds for acceptable notes in a voice
 (struct part-range [lower-bound upper-bound]
   #:guard
     (lambda (lb ub name)
@@ -31,21 +34,27 @@
 
 ;; a mapping of current states to next states
 ;; implement via pattern matching
+;; doesn't do anything
 (struct policy [stuff])
 
+
+;; dummy function
 (define (schenkerian-analysis current-state) 1)
 
 ;; pick a next state based on policy
 ;; update current state value based on analysis function reward/penalty
 ;; return the next state
+;; dummy function
 (define (state-transition current-state
                           adjacent-states
                           policy
                           analysis-function) 1)
 
+;; roman numerals represent functional harmony
 (define chord-progression
   (list 'I 'IV 'V 'I))
 
+;; example of possible voice part ranges
 (define example-part-range-list
   (list
     (part-range (pitch 'C 4) (pitch 'C 6)) ;; Soprano
@@ -53,12 +62,14 @@
     (part-range (pitch 'C 3) (pitch 'C 5)) ;; Tenor
     (part-range (pitch 'E 2) (pitch 'E 4)))) ;; Bass 
 
+;; turns part range into a list of appropriate notes
 (define (enumerate-part-range pr)
   (for/list ([i (in-range 
                   (note-midi-number (part-range-lower-bound pr))
                   (+ (note-midi-number (part-range-upper-bound pr)) 1))])
     (make-note-from-midi-num i null-beat)))
 
+;; example of C major harmony
 (define  cmaj 
   (harmony
     (whole-note 'C 5)
@@ -66,7 +77,7 @@
     (whole-note 'G 5)
     (whole-note 'C 5)))
 
-
+;; takes a part range and returns a list of playable notes in harmony
 (define (part-range-valid-pitches pr harmony)
   (let ([pr-all-pitches (enumerate-part-range pr)])
     (filter 
@@ -77,7 +88,9 @@
       pr-all-pitches)))
     
 
-
+;; populates the state space, which is a list of cartesian products of valid
+;; pitches for each part range
+;; takes a recursive approach in populating state space.  Takes first chord in state space and return all possible chord voicings.
 (define (populate-state-space progression
                               key
                               list-of-part-ranges)
@@ -102,11 +115,13 @@
           (populate-helper (cdr progression) key list-of-part-ranges (cons state-layer list-of-states)))))
   (populate-helper (reverse chord-progression) key list-of-part-ranges '()))
 
+;; creates a sample state space over I, IV, V, I progression in C major
 (define example-state-space
   (populate-state-space chord-progression
                         (pitch 'C 5)
                         example-part-range-list))
 
+;; prints state space
 (define (print-state-space stsp)
   (map
     (lambda (state-layer)
