2b3057993c6fc2d5643070b8f77e66e4561ff6ba
David Benoit <ddavidrb@gmail.com>
2016-11-27 22:24:02 -0500

    refactor policy into separate file

--- mdp-state.rkt
+++ mdp-state.rkt
@@ -3,8 +3,7 @@
 ;; needed libraries
 (require 
          "rsound-composer/define-argcheck.rkt"
-         "rsound-composer/composer.rkt"
-         "racket-avl-modified.rkt")
+         "rsound-composer/composer.rkt")
 
 (provide (all-defined-out)
          (all-from-out "rsound-composer/composer.rkt"))
@@ -61,11 +60,6 @@
           (else #f)))
   (s=helper (state-voicing-prog s1) (state-voicing-prog s2)))
 
-
-
-
-
-
 ;; create a note without duration (pitch)
 (define (pitch letter octave)
   (note letter octave null-beat))
@@ -80,94 +74,6 @@
           (values lb ub)
           (error "Invalid part-range parameters"))))
            
-
-;; a mapping of current states to next states
-;; implement via pattern matching
-(struct policy [state-mapping]
-  #:guard
-  (lambda (stm type-name)
-    (if (avl? stm)
-      stm
-      (error "Invalid policy parameter"))))
-
-(define (policy-add-mapping pol
-                            current-state
-                            next-state)
-  (avl-add! (policy-state-mapping pol)
-            (cons current-state next-state)))
-
-(define (policy-contains? pol
-                          st)
-  (avl-contains? (policy-state-mapping pol) (cons st 0)))
-
-(define (policy-match pol
-                      st)
-  (avl-find (policy-state-mapping pol) (cons st 0)))
-
-(define (policy-request-action pol
-                               st
-                               voicing-enumeration)
-  (if (not (policy-contains? pol st))
-      (policy-add-mapping pol st (state
-                                  (append (state-voicing-prog st) (list-ref voicing-enumeration (length (state-voicing-prog st))))
-                                  (state-harmonic-progression st)
-                                  0))
-      (cdr (policy-match pol st))))
-
-(define test-policy
-  (policy (make-custom-avl (lambda (x y) (state<=? (car x) (car y))) (lambda (x y) (state=? (car x) (car y))))))
-
-;; roman numerals represent functional harmony
-(define chord-progression
-  (list 'I 'IV 'V 'I))
-
-(define empty-state
-  (state '() chord-progression 0))
-
-(define state-one
-  (state (list
-           (list
-               (note 'C 4 null-beat)
-               (note 'G 3 null-beat)
-               (note 'C 3 null-beat)
-               (note 'E 2 null-beat)))
-         chord-progression
-         0))
-
-(define state-two
-  (state (list 
-           (list
-               (note 'C 4 null-beat)
-               (note 'G 3 null-beat)
-               (note 'C 3 null-beat)
-               (note 'E 2 null-beat))
-           (list 
-               (note 'A 4 null-beat)
-               (note 'F 3 null-beat)
-               (note 'C 3 null-beat)
-               (note 'A 2 null-beat)))
-         chord-progression
-         0))
-
-;(define (policy-get-action pol current-state)
-;  (let ([
-
-(policy-add-mapping test-policy empty-state state-one)
-;(policy-contains? test-policy empty-state)
-
-
-
-;; dummy function
-(define (schenkerian-analysis current-state) 1)
-
-;; pick a next state based on policy
-;; update current state value based on analysis function reward/penalty
-;; return the next state
-;; dummy function
-(define (state-transition current-state
-                          adjacent-states
-                          policy
-                          analysis-function) 1)
 
 ;; example of possible voice part ranges
 (define example-part-range-list
@@ -206,7 +112,7 @@
 ;; populates the state space, which is a list of cartesian products of valid
 ;; pitches for each part range
 ;; takes a recursive approach in populating state space.  Takes first chord in state space and return all possible chord voicings.
-(define (populate-state-space progression
+(define (populate-voicing-enumeration progression
                               key
                               list-of-part-ranges)
  (define (populate-helper sub-progression
@@ -251,15 +157,18 @@
           (populate-helper (cdr sub-progression) key list-of-part-ranges (cons voicing-group list-of-voicing-groups)))))
   (populate-helper (reverse chord-progression) key list-of-part-ranges '()))
 
+;; roman numerals represent functional harmony
+(define chord-progression
+  (list 'I 'IV 'V 'I))
+
 ;; creates a sample state space over I, IV, V, I progression in C major
 (define example-voicing-enumeration
-  (populate-state-space chord-progression
+  (populate-voicing-enumeration chord-progression
                         (pitch 'C 5)
                         example-part-range-list))
 
-
 ;; prints state space
-(define (print-state-space stsp)
+(define (print-voicing-enumeration ve)
  (void 
    (map
      (lambda (voicing-group)
@@ -271,19 +180,8 @@
               voicing)
           (newline))
        voicing-group))
-  stsp)
+  ve)
    (display "Total State Space Size: ")
-   (display (length (flatten stsp)))
+   (display (length (flatten ve)))
    (newline)))
 
-;;(define test-avl
- ;; (make-avl state-<=?))
-
-;(print-state-space example-state-space)
-
-;; The following line should return true
-(state=?
-  (policy-request-action test-policy
-                         empty-state
-                         example-voicing-enumeration)
-  state-one)
