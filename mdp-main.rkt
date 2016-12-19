582e96ecce23722fcedd746b9e2e5107019c828f
David Benoit <ddavidrb@gmail.com>
2016-12-19 09:08:16 -0500

    update

--- mdp-main.rkt
+++ mdp-main.rkt
@@ -1,46 +1,46 @@
 #lang racket
 
 (require "mdp-state.rkt"
-         "mdp-reward.rkt"
-         "rsound-composer/composer.rkt")
+	 "mdp-reward.rkt"
+	 "rsound-composer/composer.rkt")
 
 (provide (all-defined-out)
-         (all-from-out 
-           "mdp-state.rkt"
-           "mdp-reward.rkt"))
+	 (all-from-out 
+	   "mdp-state.rkt"
+	   "mdp-reward.rkt"))
 
 (define (value-iteration state-table rec-depth)
   (if (<= rec-depth 0)
     state-table
     (let* ([min-length (apply min (map length state-table))]
-           [sub-state-space (map (lambda (state-group)
-                                   (sort 
-                                     (take state-group min-length)
-                                     (lambda (x y)
-                                       (< (state-value x) (state-value y))))
-                                   ) 
-                                 state-table)]
-           [iteration-table
-             (map* list sub-state-space)]
+	   [sub-state-space (map (lambda (state-group)
+				   (sort 
+				     (take state-group min-length)
+				     (lambda (x y)
+				       (< (state-value x) (state-value y))))
+				   ) 
+				 state-table)]
+	   [iteration-table
+	     (map* list sub-state-space)]
 
-           [updated-state-space
-             (map* list
-                   (map reward-function iteration-table))])
+	   [updated-state-space
+	     (map* list
+		   (map reward-function iteration-table))])
 
       (value-iteration updated-state-space (- rec-depth 1)))))
 
 ;(print-state-space (value-iteration example-state-space))
 
 (define cm (convolution-matrix (car (value-iteration example-state-space 1))
-                               1))
+			       1))
 
 (define (display-convolution-matrix m)
   (map (lambda (lst)
-         (map (lambda (n)
-                (display (cons (note-letter n) (note-octave n)))
-                (newline))
-              lst)
-         (newline))
+	 (map (lambda (n)
+		(display (cons (note-letter n) (note-octave n)))
+		(newline))
+	      lst)
+	 (newline))
        m))
 
 (define (best-path state-space)
@@ -48,16 +48,31 @@
 
 (define (play-markov-path path-list)
   (play-instrument-part #:tempo 100
-                        (instrument-part 
-                          [vgame-synth-instrument 4]
-                          (apply measure
-                                 (map 
-                                   (lambda (st)
-                                     (apply harmony
-                                            (map (lambda (n)
-                                                   (quarter-note (note-letter n) (note-octave n)))
-                                                 (state-voicing st))))
-                                   path-list)))))
+			(instrument-part 
+			  [vgame-synth-instrument 4]
+			  (apply measure
+				 (map 
+				   (lambda (st)
+				     (apply harmony
+					    (map (lambda (n)
+						   (quarter-note (note-letter n) (note-octave n)))
+						 (state-voicing st))))
+				   path-list)))))
+
+(define (export-markov-path path-list filename)
+  (rs-write
+    (instrument-part->rsound #:tempo 100
+			     (instrument-part 
+			       [vgame-synth-instrument 4]
+			       (apply measure
+				      (map 
+					(lambda (st)
+					  (apply harmony
+						 (map (lambda (n)
+							(quarter-note (note-letter n) (note-octave n)))
+						      (state-voicing st))))
+					path-list))))
+    filename))
 
 (define progression1 (list 'I 'IV 'V 'I))
 (define progression2 (list 'i 'bVI 'iv 'ii-dim 'V 'i))
@@ -65,13 +80,33 @@
 (define progression4 (list 'I 'bVII 'bIII 'ii-dim 'V 'I))
 (define progression5 (list 'I 'IV 'I 'V 'vi 'IV 'V 'I))
 
+(define (progression->state-space progression)
+	  (map 
+	    shuffle
+	    (populate-state-space progression
+				  (eighth-note 'C 5)
+				  example-part-range-list)))
+
+(define state-space1
+	  (progression->state-space progression1))
+(define state-space2
+	  (progression->state-space progression2))
+(define state-space3
+	  (progression->state-space progression1))
+(define state-space4
+	  (progression->state-space progression4))
+(define state-space5
+	  (progression->state-space progression5))
+
+
+
 (define (run-markov-system-trial key progression)
   (let ([state-space 
-          (map 
-            shuffle
-            (populate-state-space progression
-                                  key
-                                  example-part-range-list))])
+	  (map 
+	    shuffle
+	    (populate-state-space progression
+				  key
+				  example-part-range-list))])
     (display "Running Markov System Trial\n\n")
     (display "Chord Progression: ")
     (display progression)
@@ -93,5 +128,5 @@
     (map display-state (best-path (value-iteration state-space 1000)))
     (newline)
     (newline)))
-(run-markov-system-trial (eighth-note 'C 5) progression5)
+;(run-markov-system-trial (eighth-note 'C 5) progression5)
 
